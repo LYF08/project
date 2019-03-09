228699d8702c40335e475a65ff29ab392d24b2d3
Yius <541443423@qq.com>
2019-03-09 23:07:52 +0800

    激动过头，忘记传新增的php文件了，现在补传

--- dormitoryPHP/createNewSign.php
+++ dormitoryPHP/createNewSign.php
@@ -3,13 +3,19 @@
     $Rtime=$_POST['Rtime'];//APP post过来的签到日期
     $houseparentID=$_POST['houseparentID'];//APP post过来的宿管号
     $title=$_POST['title'];//APP post过来的签到标题
-    $result=mysqli_query($conn,"INSERT INTO signrecord (Rtime,houseparentID,title) VALUES('$Atime','$houseparentID','$title')");
+    $govern=$_POST['govern'];//APP post过来的宿管管理的楼号
+    $sql=mysqli_query($conn,"SELECT COUNT(*) as 'totalnums' FROM userinfo WHERE belong LIKE '$govern'");
+    $totalnums=0;
+    if($result=mysqli_fetch_assoc($sql)){
+        $totalnums=$result['totalnums'];
+    }
+    $result=mysqli_query($conn,"INSERT INTO signrecord (Rtime,houseparentID,title,govern,totalnums) VALUES('$Rtime','$houseparentID','$title','$govern','$totalnums')");
      if($result){
-       $back['status']="8";
+       $back['status']="13";
        $back['info']="start a new sign success";
        echo (json_encode($back));
     }else{
-       $back['status']="-11";
+       $back['status']="-16";
        $back['info']="start a new sign fail";
        echo (json_encode($back));
     }
--- dormitoryPHP/signTimeRecord.php
+++ dormitoryPHP/signTimeRecord.php
@@ -5,12 +5,20 @@
     $signedtime=$_POST['signedtime'];//APP post过来的签到时间
     $result=mysqli_query($conn,"INSERT INTO signed (SID,recordID,signedtime) VALUES('$SID',$recordID,'$signedtime')");
      if($result){
-       $back['status']="9";
-       $back['info']="sign success";
-       echo (json_encode($back));
+     $result2=mysqli_query($conn,"UPDATE signrecord SET nums=nums+1 WHERE ID = '$recordID'");
+       if($result2){
+            $back['status']="14";
+            $back['info']="sign success";
+            echo (json_encode($back));
+       }else{
+           //其实这里应该用事务回滚，懒就不写了
+           $back['status']="-18";
+           $back['info']="sign fail, can not update";
+           echo (json_encode($back));
+       }
     }else{
-       $back['status']="-12";
-       $back['info']="sign fail";
+       $back['status']="-17";
+       $back['info']="sign fail, can not sign";
        echo (json_encode($back));
     }
     mysqli_close($conn);
