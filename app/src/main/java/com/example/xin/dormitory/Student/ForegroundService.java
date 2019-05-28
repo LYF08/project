f6360ba4bc9eafde7fd246f8d9e74aff77a39adb
Yius <541443423@qq.com>
2019-05-28 17:01:28 +0800

    修复了android 8以上前台服务不显示的问题

--- app/src/main/java/com/example/xin/dormitory/Student/ForegroundService.java
+++ app/src/main/java/com/example/xin/dormitory/Student/ForegroundService.java
@@ -1,9 +1,12 @@
 package com.example.xin.dormitory.Student;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
@@ -20,12 +23,29 @@ public class ForegroundService extends Service {
         super.onCreate();
         Intent intent =  new Intent(this,PostsListActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
-        Notification notification = new NotificationCompat.Builder(this)
-                .setContentTitle("学生宿舍管理系统")
-                .setContentText("有新鲜事？发个帖子聊天吧！")
-                .setSmallIcon(R.drawable.ic_room)
-                .setContentIntent(pendingIntent)
-                .build();
+        Notification notification;
+        //Android 8.0以上专门要做适配
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String channelID = "ForegroundService";
+            NotificationChannel channel = new NotificationChannel(channelID,"前台发帖服务",NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+            notification = new NotificationCompat.Builder(this,channelID)
+                    .setContentTitle("学生宿舍管理系统")
+                    .setContentText("有新鲜事？发个帖子聊天吧！")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pendingIntent)
+                    .build();
+        }else{
+            notification = new NotificationCompat.Builder(this)
+                    .setContentTitle("学生宿舍管理系统")
+                    .setContentText("有新鲜事？发个帖子聊天吧！")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(pendingIntent)
+                    .build();
+        }
+
+
         startForeground(1,notification);
     }
 
